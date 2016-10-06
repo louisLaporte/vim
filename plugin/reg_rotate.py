9ac3ea679f641e0e1ff5c852b54435b059986a60
louis laporte <louis.laporte12@gmail.com>
2016-10-05 23:22:46 -0400

    Create a separated python file

---
+++ plugin/reg_rotate.py
@@ -0,0 +1,59 @@
+import vim
+import sys
+import time
+class CUtility(object):
+    @classmethod
+    def for_loop(cls):
+        text=\
+"""for(;;) {
+
+}
+"""
+        return text
+    @classmethod
+    def while_loop(cls):
+        text=\
+"""while() {
+
+}
+"""
+        return text
+
+    @classmethod
+    def switch_case(cls):
+        text=\
+"""switch () {
+    case :
+
+    break;
+    default:
+    
+    break;
+}
+"""
+        return text
+
+class CFunction(object):
+    @classmethod
+    def mainFunction(cls):
+        text=\
+"""#include <stdio.h>
+#include<stdlib.h>
+int main(void) {
+
+    return 0;
+}
+"""
+        return text
+class CInsert(object):
+    def __init__(self):
+        self.w = vim.current.window
+        self.b = vim.current.buffer
+        self.l = vim.current.line
+    def run(self):
+             print "hello\nhello2\n\nhello3" 
+             cc=CUtility.for_loop()
+             l=len(cc.switch_case().split('\n'))
+             t = cc.switch_case().split('\n')
+             r,c = self.w.cursor
+             self.b[r:l-1]=t
